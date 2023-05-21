1ffe2c95d7f620add1d32d6c87e5b20bab6c6b30
Adnan Malkani <adnanmalkani@hotmail.com>
2023-05-21 01:46:39 +0100

    No change

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,29 @@
-import { useState } from "react";
-import ExpenseList from "./expense-tracker/components/ExpenseList";
-import ExpenseFilter from "./expense-tracker/components/ExpenseFilter";
-import ExpenseForm from "./expense-tracker/components/ExpenseForm";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
-export const categories = ["Utilites", "Entertainment", "Groceries"];
+interface User {
+  id: number;
+  name: string;
+}
 
 function App() {
-  const [selectedCategory, setSelectedCategory] = useState("");
-
-  const [expenses, setExpenses] = useState([
-    { id: 1, description: "aaa", amount: 1, category: "Utilites" },
-    { id: 2, description: "bbb", amount: 1, category: "Entertainment" },
-    { id: 3, description: "ccc", amount: 1, category: "Groceries" },
-    { id: 4, description: "ddd", amount: 1, category: "Groceries" },
-  ]);
-  if (expenses.length === 0) return null;
-
-  const visibleExpenses = selectedCategory
-    ? expenses.filter((e) => e.category === selectedCategory)
-    : expenses;
+  const [users, setUsers] = useState<User[]>([]);
+  const [error, setError] = useState("");
+  useEffect(() => {
+    axios
+      .get<User[]>("https://jsonplaceholder.typicode.com/users")
+      .then((res) => setUsers(res.data))
+      .catch((err) => setError(err.message));
+  }, []);
 
   return (
     <>
-      <div className="mb-3">
-        <ExpenseForm />
-      </div>
-      <div className="mb-3">
-        <ExpenseFilter
-          onSelectCategory={(category) => setSelectedCategory(category)}
-        />
-      </div>
-      <ExpenseList
-        expenses={visibleExpenses}
-        onDelete={(id) => setExpenses(expenses.filter((e) => e.id !== id))}
-      />
+      {error && <p className="text-danger">{error}</p>}
+      <ul>
+        {users.map((user) => (
+          <li key={user.id}>{user.name}</li>
+        ))}
+      </ul>
     </>
   );
 }
